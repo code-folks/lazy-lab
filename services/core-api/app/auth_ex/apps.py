1e38bc34b9cca1ec4bf356c033c46d6d5518c983
Pawel Haly <halypawel@lazylab.solutions>
2023-04-14 17:20:07 +0200

    Add pre-commit config.

--- services/core-api/app/auth_ex/apps.py
+++ services/core-api/app/auth_ex/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class AuthExConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auth_ex'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "auth_ex"
