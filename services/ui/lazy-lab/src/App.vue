419618d6480e62ec4f719e1091d2abadf875ccf0
dexax <d.exax@hotmail.com>
2023-04-16 13:00:30 +0200

    Add lang declaration

--- services/ui/lazy-lab/src/App.vue
+++ services/ui/lazy-lab/src/App.vue
@@ -19,11 +19,11 @@
         <div class="-space-y-px rounded-md shadow-sm">
           <div>
             <label for="email-address" class="sr-only">Email address</label>
-            <input id="email-address" name="email" type="email" autocomplete="email" required="" class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email address" />
+            <input id="email-address" name="email" type="email" autocomplete="email" required="true" class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email address" />
           </div>
           <div>
             <label for="password" class="sr-only">Password</label>
-            <input id="password" name="password" type="password" autocomplete="current-password" required="" class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password" />
+            <input id="password" name="password" type="password" autocomplete="current-password" required="true" class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password" />
           </div>
         </div>
 
@@ -51,6 +51,6 @@
   </div>
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import { LockClosedIcon } from '@heroicons/vue/20/solid'
 </script>
