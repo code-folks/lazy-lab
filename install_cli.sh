442c81dbd96b8a690e8870567fc9b6c50680f324
rskiba <radoslaw.skiba@useindio.com>
2023-04-29 23:06:08 +0200

    Update install_cli prompts to match branding

--- install_cli.sh
+++ install_cli.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-echo ">>  Installing 🌊 so your 🐋 can start swimming."
+echo "- Putting all the 🔬 on table."
 pip install -r ./cli/requirements.txt --exists-action s --quiet &> /dev/null
-echo ">>  Making new command executable."
+echo "- Making new command executable."
 chmod --reference=install_cli.sh ./lab
 ./lab --install-completion &> /dev/null
-echo ">>  Success... ✅"
-echo -e ">>  Use \033[1;36m./lab --help\033[0;0m to check what you can do."
-echo -e ">>  📬 Restart shell to use auto-completition for a new CLI."
+echo -e "\033[1;32mSuccess! remember to wear your 🥼 in labolatory.\033[0;0m"
+echo "Restart shell to use auto-completition for a new CLI."
+echo -e "Use \033[1;36m./lab --help\033[0;0m to check what you can do."
